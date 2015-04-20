22df8e7ecf3a9e92898030a8737f13e339254228
dml337 <dml337@cornell.edu>
2015-04-19 22:49:14 -0400

    updated datestr filename

--- WriteRealData.py
+++ WriteRealData.py
@@ -33,7 +33,8 @@ s.Calibrate()
 
 #filename = 'Tests.txt'
 #file = open(filename,'a')
-datestr = '#Test ' + time.strftime("%c")
+datestr = 'Test ' + time.strftime("%c")
+datestr = 'Test_' + time.strftime("%d_%b_%Y_%H:%M:%S", time.gmtime())
 filename = datestr
 
 #dt = 0.02
