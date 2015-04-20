d4672422674ea98b1a26c2e79dfe8c526802f175
dml337 <dml337@cornell.edu>
2015-04-19 22:41:38 -0400

    updated gyroPlotterBasic to output time as well

--- gyroPlotterBasic.py
+++ gyroPlotterBasic.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 
 filename = sys.argv[1]
 
-#t, x, y, z = np.loadtxt(filename)
-print np.loadtxt(filename)
-x, y, z = np.loadtxt(filename, unpack=True)
+t, x, y, z = np.loadtxt(filename)
+#print np.loadtxt(filename)
+#x, y, z = np.loadtxt(filename, unpack=True)
 
 tstep = 0.02
 t = tstep*np.linspace(0, len(x), len(x))
@@ -20,14 +20,14 @@ t = tstep*np.linspace(0, len(x), len(x))
 plt.subplot(3,1,1)
 plt.plot(t,x,'r-')
 plt.title('Angular Velocity Measurements')
-plt.ylabel('Angular Rate x (dps)')
+plt.ylabel('x (dps)')
 
 plt.subplot(3,1,2)
 plt.plot(t,y,'g-')
-plt.ylabel('Anglar Rate y (dps)')
+plt.ylabel('y (dps)')
 
 plt.subplot(3,1,3)
 plt.plot(t,z,'b-')
-plt.ylabel('Angular Rate z (dps)')
+plt.ylabel('z (dps)')
 
 plt.show()
